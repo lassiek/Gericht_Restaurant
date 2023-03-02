862aa87ed3cadbab7c3946308cdb6039d12449f1
lassiek <korshunova22198@mail.ru>
2023-03-03 00:43:32 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+// const a = +prompt('Введите сторону а')
+// const b = +prompt('Введите сторону b')
+// const c = +prompt('Введите сторону c')
+// let p = a + b + c
+// let s = 1/2 * a * b
+// alert (`Периметр = ${p}, Площадь = ${s}`)
+
+// const q = [3, 6, 2, 6 ,7, 9 ,9, 9]
+
+// for(let i = 0; i < q.length; i++) {
+//     console.log(q[i])
+// }
+
+
+
+// q.forEach((el) => {
+//     console.log(el)
+// })
+
+// const d = 7
+// console.log (d)
+// console.log (Math.pow(d,2))
+// console.log (Math.pow(d,3))
+
+// const a = prompt ('Введите ваш год рождения')
+// const BD = new Date (a)
+// const y = BD.getFullYear()
+// const currentDate = new Date().getFullYear()
+// console.log (y)
+// console.log (currentDate)
+// console.log (currentDate - y)
+
+// let userBirthday = prompt ('Введите дату рождения')
+
+// userBirthday = new Date(userBirthday)
+
+// const dd = Number(userBirthday.getDate())
+// const mm = Number(userBirthday.getMonth()) 
+// const yyyy = Number(userBirthday.getFullYear())
+
+// const currentdd = new Date().getDate()
+// const currentmm = new Date().getMonth()
+// const currentyyyy = new Date().getFullYear()
+
+
+// if (currentyyyy - yyyy == 14 || currentyyyy - yyyy == 20 || currentyyyy - yyyy == 45) {
+//     alert (`Вам необходимо заменить паспорт не позднее ${dd}/${mm + 2}/${currentyyyy}`)
+// }
+// if ((currentyyyy - yyyy > 14 && currentyyyy - yyyy < 20) || currentyyyy - yyyy > 45) {
+//     alert ('Вам не нужно менять паспорт')
+// }
+// if (currentyyyy - yyyy) {
+//     alert (`Вам нужно получить паспорт через ${14 - (currentyyyy - yyyy)} лет`)
+// }
